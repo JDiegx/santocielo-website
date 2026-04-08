bb07d690325409d58681501f578bdac6b4a53d81
Juan Diego Sánchez <juandiegosanchex21@gmail.com>
2026-04-08 01:57:28 -0500

    update 2

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import Router from './router/router'
+import Router from './router/index'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
