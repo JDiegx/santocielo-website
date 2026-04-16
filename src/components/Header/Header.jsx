49affe17af5034d6720eae6e6c1cf28accc046a9
Juan Diego Sánchez <juandiegosanchex21@gmail.com>
2026-04-16 12:27:13 -0500

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
-        <header className='w-full h-[50px] flex items-center justify-center fixed backdrop-blur-md bg-white/30 z-10 top-0'>
+        <header className='w-full h-[50px] flex items-center justify-center fixed backdrop-blur-md bg-white/40 z-10 top-0'>
             <Link to={"/"}>
                 <img src="/img/santocielo-logo.svg" alt="Logo of Santo Cielo!" className='w-[50px]' />
             </Link>
