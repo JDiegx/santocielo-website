23788d83cc884fb1c28146f8b9021c0028a8af97
Juan Diego Sánchez <juandiegosanchex21@gmail.com>
2026-09-15 01:34:52 -0500

    update pdf compress

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,45 +1,17 @@
-import React, { useEffect, useState } from 'react'
-import { Document, Page, pdfjs } from 'react-pdf'
+import React from 'react'
 import Header from '../../components/Header/Header'
 
-import 'react-pdf/dist/Page/AnnotationLayer.css'
-import 'react-pdf/dist/Page/TextLayer.css'
-
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`
-
 const Menu = () => {
-    const [numPages, setNumPages] = useState(null)
-    const [width, setWidth] = useState(window.innerWidth)
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWidth(window.innerWidth)
-        }
-
-        window.addEventListener('resize', handleResize)
-
-        return () => {
-            window.removeEventListener('resize', handleResize)
-        }
-    }, [])
-
     return (
         <>
             <Header />
 
-            <section className="w-full flex justify-center mt-[60px]">
-                <Document
-                    file="/pdf/menu-santocielo-new.pdf"
-                    onLoadSuccess={({ numPages }) => setNumPages(numPages)}
-                >
-                    {Array.from(new Array(numPages), (_, index) => (
-                        <Page
-                            key={`page_${index + 1}`}
-                            pageNumber={index + 1}
-                            width={width}
-                        />
-                    ))}
-                </Document>
+            <section className="w-full mt-[60px]">
+                <iframe
+                    src="/pdf/menu-santocielo-new.pdf"
+                    title="Menú de Santo Cielo"
+                    className="w-full h-[calc(100vh-60px)] border-0"
+                />
             </section>
         </>
     )
