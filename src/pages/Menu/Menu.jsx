6cde11cb63b0289709fdd96636a4a4f69d6d7ea1
Juan Diego Sánchez <juandiegosanchex21@gmail.com>
2026-09-15 02:14:37 -0500

    update

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,24 +1,55 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import {
+    TransformWrapper,
+    TransformComponent,
+} from 'react-zoom-pan-pinch'
 
 const Menu = () => {
+    const pages = [
+        '/menu/1.webp',
+        '/menu/2.webp',
+    ]
+
     return (
         <>
             <Header />
 
             <main className="w-full pt-[60px] bg-white">
-                <div className="w-full max-w-4xl mx-auto">
-                    <img
-                        src="/menu/1.webp"
-                        alt="Menú Santo Cielo - Página 1"
-                        className="block w-full h-auto"
-                    />
-
-                    <img
-                        src="/menu/2.webp"
-                        alt="Menú Santo Cielo - Página 2"
-                        className="block w-full h-auto"
-                    />
+                <div className="w-full">
+                    {pages.map((page, index) => (
+                        <TransformWrapper
+                            key={page}
+                            initialScale={1}
+                            minScale={1}
+                            maxScale={4}
+                            centerOnInit
+                            doubleClick={{
+                                mode: 'zoomIn',
+                            }}
+                            pinch={{
+                                disabled: false,
+                            }}
+                            wheel={{
+                                disabled: true,
+                            }}
+                            panning={{
+                                disabled: false,
+                            }}
+                        >
+                            <TransformComponent
+                                wrapperClass="!w-full"
+                                contentClass="!w-full"
+                            >
+                                <img
+                                    src={page}
+                                    alt={`Menú Santo Cielo - Página ${index + 1}`}
+                                    className="block w-full h-auto"
+                                    draggable={false}
+                                />
+                            </TransformComponent>
+                        </TransformWrapper>
+                    ))}
                 </div>
             </main>
         </>
